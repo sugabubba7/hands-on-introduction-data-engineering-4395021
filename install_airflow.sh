de2792414961f42781f9cfec33b449518c110d57
Sumit Singh <109894162+sugabubba7@users.noreply.github.com>
2025-02-27 17:37:15 +0000

    02-27-2025

--- install_airflow.sh
+++ install_airflow.sh
@@ -18,7 +18,7 @@ PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 echo "This script will install Airflow version $AIRFLOW_VERSION, with Python $PYTHON_VERSION."
 sleep 3
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
-pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+pip install "apache-airflow==2.6.3" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.6.3/constraints-3.10.txt"
 
 echo "Running DB Migrate"
 
